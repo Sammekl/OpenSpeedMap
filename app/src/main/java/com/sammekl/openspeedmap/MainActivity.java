632854bfda815b1f17104db0e466360aad911c76
Samme <sammekleijn@gmail.com>
2015-06-20 12:00:33 +0200

    Main layout

--- app/src/main/java/com/sammekl/openspeedmap/MainActivity.java
+++ app/src/main/java/com/sammekl/openspeedmap/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
     }
 
     @Override
